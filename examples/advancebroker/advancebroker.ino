6e3064864731a0b49f3827e6abfe0c6d2eb834ed
terror <terror85@gmail.com>
2021-10-29 10:29:09 +0300

    Implemented #2

--- examples/advancebroker/advancebroker.ino
+++ examples/advancebroker/advancebroker.ino
@@ -11,15 +11,35 @@ public:
 	void onRemove(sMQTTClient*)
     {
     };
+    void onPublish(const std::string &topic, const std::string &payload)
+    {
+        // client publish a message to the topic
+        // you can parse the message, save it to a database, or whatever, based on your goals
+    }
 };
 
 MyBroker broker;
 
 void setup()
 {
-    broker.init(1883);
+    Serial.begin(115200);
+    const char* ssid = "SSID";         // The SSID (name) of the Wi-Fi network you want to connect to
+    const char* password = "PASSWORD"; // The password of the Wi-Fi network
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+        delay(1000);
+    }
+    Serial.println("Connection established!");  
+    Serial.print("IP address:\t");
+    Serial.println(WiFi.localIP());
+    
+    const unsigned short mqttPort=1883;
+    broker.init(mqttPort);
+    // all done
+    // your magic code
 };
 void loop()
 {
     broker.update();
+    // your magic code
 };
--- examples/simplebroker/simplebroker.ino
+++ examples/simplebroker/simplebroker.ino
@@ -4,7 +4,20 @@ sMQTTBroker broker;
 
 void setup()
 {
-    broker.init(1883);
+    Serial.begin(115200);
+    const char* ssid = "SSID";         // The SSID (name) of the Wi-Fi network you want to connect to
+    const char* password = "PASSWORD"; // The password of the Wi-Fi network
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) { // Wait for the Wi-Fi to connect
+        delay(1000);
+    }
+    Serial.println("Connection established!");  
+    Serial.print("IP address:\t");
+    Serial.println(WiFi.localIP());
+    
+    const unsigned short mqttPort=1883;
+    broker.init(mqttPort);
+    // all done
 }
 void loop()
 {
